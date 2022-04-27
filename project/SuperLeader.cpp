bce0cbd3c9737d8ed3e09707d0f5dab960e5103d
Anna <anutalisovec@gmail.com>
2022-04-27 18:41:07 +0300

    a little update classes(LEADER AND SUPERlEADER)

--- project/SuperLeader.cpp
+++ project/SuperLeader.cpp
@@ -1,1 +1,62 @@
 #include "SuperLeader.h"
+#include "Home.h"
+
+SuperLeader::SuperLeader() {
+    setMaterial("no material");
+    setCountOfFloors(1);
+    setNumberOfInhabitants(1);
+}
+
+//constructor of Student with params
+SuperLeader::SuperLeader(string material, int countOfFloors, int numberOfInhabitants, string abiluty) {
+    setMaterial("no material");
+    setCountOfFloors(1);
+    setNumberOfInhabitants(1);
+    setAbiluty("no abiluty");
+}
+
+//destructor of Student
+SuperLeader::~SuperLeader() {
+
+}
+
+string SuperLeader::SuperLeader() {
+    return material;
+}
+
+void SuperLeader::setMaterial(string material) {
+    this->material = material;
+}
+
+int SuperLeader::getCountOfFloors() {
+    return countOfFloors;
+}
+
+void SuperLeader::setCountOfFloors(int countOfFloors) {
+    if (countOfFloors >= 1 && countOfFloors <= 100) {
+        this->countOfFloors = countOfFloors;
+    }
+}
+
+int SuperLeader::getNumberOfInhabitants() {
+    return numberOfInhabitants;
+}
+
+void SuperLeader::setNumberOfInhabitants(int numberOfInhabitants) {
+    if (numberOfInhabitants >= 0 && numberOfInhabitants <= 1000) {
+        this->numberOfInhabitants = numberOfInhabitants;
+    }
+}
+
+string getAbiluty() {
+    return abiluty;
+}
+void setAbiluty(string abiluty) {
+    this->abiluty = abiluty;
+}
+
+string SuperLeader::getInfo() {
+    return "";
+}
+
+
--- project/leader.cpp
+++ project/leader.cpp
@@ -1,2 +1,30 @@
 #include "Leader.h"
 
+#include "Home.h"
+
+Leader::Leader() {
+    setMaterial ("no material");
+    setCountOfFloors(1);
+    setNumberOfInhabitants(1);
+}
+
+//constructor of Student with params
+Leader::Leader(string material, int countOfFloors, int numberOfInhabitants, string phoneNumber, string e_mail) {
+    setMaterial(material);
+    setCountOfFloors(countOfFloors);
+    setNumberOfInhabitants(numberOfInhabitants);
+    setPhoneNumber(phoneNumber);
+    setEmail(e_mail);
+}
+
+//destructor of Student
+Leader::~Leader() {
+
+}
+
+string Leader::getInfo() {
+    return "";
+}
+
+
+
